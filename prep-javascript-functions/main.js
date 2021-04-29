ecea911b2e125b7e2b3c5402a5b4a3602ae6ad35
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-04-29 17:40:23 +0300

    Completed convertHoursToMinutes.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,19 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+var sum = addTwoNumbers(2, 2);
+console.log('sum', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var minutes = convertHoursToMinutes(2);
+console.log('minutes', minutes);
