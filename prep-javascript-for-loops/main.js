bc0883eca9e44a622b735b1228502382726b3ff7
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-05-01 12:08:20 +0300

    prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+
+for (var number1To9 = 0; number1To9 < 10; number1To9++) {
+  console.log(number1To9);
+}
+
+for (var x = 0; x < 20; x += 2) {
+  console.log(x);
+}
+
+for (var t = 100; t > 0; t--) {
+  console.log('Time till explosion ' + t + '!');
+}
